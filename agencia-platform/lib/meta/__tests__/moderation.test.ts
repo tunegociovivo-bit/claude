1508d491eb78db734069fa4ac6fa9f2bdae51697
tunegociovivo-bit <tunegociovivo@gmail.com>
2026-09-08 14:12:21 +0200

    test(meta): reproduce composite Facebook comment deletion

--- agencia-platform/lib/meta/__tests__/moderation.test.ts
+++ agencia-platform/lib/meta/__tests__/moderation.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { assertMetaDeletionConfirmed } from "@/lib/meta/comments";
+import { assertMetaDeletionConfirmed, metaDeletionObjectIds } from "@/lib/meta/comments";
 
 describe("confirmacion de borrado de comentarios en Meta", () => {
   it("acepta exclusivamente la confirmacion explicita de Meta", () => {
@@ -8,5 +8,13 @@ describe("confirmacion de borrado de comentarios en Meta", () => {
 
   it.each([{}, { success: false }, null, true])("rechaza una respuesta ambigua: %j", (response) => {
     expect(() => assertMetaDeletionConfirmed(response)).toThrow(/no confirmó/i);
+  });
+
+  it("prueba también el id final de comentarios compuestos de Facebook", () => {
+    expect(metaDeletionObjectIds("1074487091370042_1234378315234491", "facebook")).toEqual([
+      "1074487091370042_1234378315234491",
+      "1234378315234491"
+    ]);
+    expect(metaDeletionObjectIds("17890000000001", "instagram")).toEqual(["17890000000001"]);
   });
 });
