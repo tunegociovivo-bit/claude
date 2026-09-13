33b11cb2e3049dd320295a104c865b7ccddf83db
tunegociovivo-bit <tunegociovivo@gmail.com>
2026-09-13 17:55:30 +0200

    test: reject unverified Facebook search targets

--- agencia-platform/components/mobile/__tests__/facebook-android-ui.test.ts
+++ agencia-platform/components/mobile/__tests__/facebook-android-ui.test.ts
@@ -3,9 +3,7 @@ import {
   extractFacebookMembershipQuestions,
   findFacebookGroupJoinTarget,
   findFacebookMembershipState,
-  findFacebookSearchImeTarget,
-  getAndroidImeSearchFallbackPoint,
-  parseAndroidDisplaySize
+  findFacebookSearchImeTarget
 } from "@/components/mobile/facebook-android-ui";
 
 describe("Facebook Android UI", () => {
@@ -60,18 +58,20 @@ describe("Facebook Android UI", () => {
 
   it("selects the keyboard search action instead of Facebook's header search control", () => {
     const hierarchy = `<hierarchy>
-      <node text="" content-desc="Buscar" class="android.view.View" bounds="[1000,40][1080,120]" />
-      <node text="Franquicias en España" class="android.widget.EditText" focused="true" bounds="[80,40][990,120]" />
-      <node text="" content-desc="Buscar" class="android.inputmethodservice.Keyboard$Key" bounds="[960,2150][1080,2280]" />
+      <node text="" content-desc="Buscar" package="com.facebook.katana" class="android.view.View" bounds="[1000,40][1080,120]" />
+      <node text="Franquicias en España" package="com.facebook.katana" class="android.widget.EditText" focused="true" bounds="[80,40][990,120]" />
+      <node text="" content-desc="Buscar" package="com.google.android.inputmethod.latin" class="android.inputmethodservice.Keyboard$Key" bounds="[960,2150][1080,2280]" />
     </hierarchy>`;
 
     expect(findFacebookSearchImeTarget(hierarchy)).toEqual({ x: 1020, y: 2215 });
   });
 
-  it("calculates a safe IME search fallback from Android's effective display size", () => {
-    expect(parseAndroidDisplaySize("Physical size: 1080x2340\nOverride size: 1080x2400"))
-      .toEqual({ width: 1080, height: 2400 });
-    expect(getAndroidImeSearchFallbackPoint({ width: 1080, height: 2400 }))
-      .toEqual({ x: 1015, y: 2196 });
+  it("refuses a lower-page search control that does not belong to an input method", () => {
+    const hierarchy = `<hierarchy>
+      <node text="Franquicias" package="com.facebook.katana" class="android.widget.EditText" focused="true" bounds="[80,40][990,120]" />
+      <node text="Buscar" package="com.facebook.katana" class="android.view.View" bounds="[900,1900][1080,2000]" />
+    </hierarchy>`;
+
+    expect(findFacebookSearchImeTarget(hierarchy)).toBeNull();
   });
 });
